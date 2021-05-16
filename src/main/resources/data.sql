a2f1c61432082e0162bb3eac9ac2ab2176c00368
Hazrat Ali <iloveyii@yahoo.com>
2021-05-16 12:00:21 +0200

    Fix in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO QUESTION (`name`, phone, email, address, language_from, language_to, question, status)
+INSERT INTO USER (`name`, email, passwordd)
 VALUES
-('abc', '123456', 'abc@mail.com', 'address 1', 'sv', 'en', 'Meeting time',1),
-('abcd', '2123456', 'abcd@mail.com', 'address 2', 'ar', 'sv', 'Meeting time 2',1);
+('root', 'root@localhost', 'root'),
+('admin', 'admin@localhost', 'admin');
